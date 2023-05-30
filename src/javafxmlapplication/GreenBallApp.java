c8967feef1f27ad28a6a676c15d3b8a5f281091d
Fran <frabamar14@gmail.com>
2023-05-30 03:47:15 +0200

    Armonizadas la vista entre ventanas

--- src/javafxmlapplication/GreenBallApp.java
+++ src/javafxmlapplication/GreenBallApp.java
@@ -8,6 +8,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import model.Club;
 import model.ClubDAOException;
@@ -40,6 +41,7 @@ public class GreenBallApp extends Application {
         }
 
         mainScene = new Scene(ROOTS.get(Scenes.INICIO));
+        stage.getIcons().add(new Image("/images/logo.png"));
         stage.setScene(mainScene);
         stage.setTitle("GreenBall");
         stage.show();
